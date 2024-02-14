01340dc2b0d54cbe79a51f889a11b0990599de7b
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-02-14 20:49:41 -0300

    fix line

---
+++ src/modules/users/routes/passwords.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express'
+import { celebrate, Joi, Segments } from 'celebrate'
+import ForgotPasswordController from '../controllers/ForgotPasswordController'
+import ResetPasswordController from '../controllers/ResetPasswordController'
+
+
+const passwordsRouter = Router()
+const passwordController = new ForgotPasswordController()
+const resetController = new ResetPasswordController()
+
+
+passwordsRouter.post('/forgot', celebrate({
+  [Segments.BODY]: {
+    email: Joi.string().email().required()
+  }
+}),
+  passwordController.create
+)
+
+passwordsRouter.post('/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      passwordConfirmation: Joi.string().required().valid(Joi.ref('password'))
+    }
+  }),
+  resetController.create
+)
+
+
+export default passwordsRouter;
+
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import productsRouter from "@modules/products/routes/products.routes";
 import sessionRouter from "@modules/users/routes/sessions.routes";
 import usersRouter from "@modules/users/routes/users.routes";
+import passwordsRouter from "@modules/users/routes/passwords.routes";
 
 const routes = Router();
 
 routes.use("/products", productsRouter);
 routes.use("/users", usersRouter);
 routes.use("/sessions", sessionRouter);
+routes.use('/password', passwordsRouter)
 
 export default routes;
